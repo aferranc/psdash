a7899384cffde42424eb74b8a92b3bdeb4218f17
Albert Ferran Casas <aferranc@cirsa.com>
2022-06-21 10:34:08 +0200

    update python packages

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "Flask==1.1.2",
+        "Flask==2.1.2",
         "psutil==5.7.2",
         "glob2==0.7",
-        "gevent==20.6.2",
+        "gevent==21.12.0",
         "zerorpc==0.6.3",
         "netifaces==0.10.9",
         "argparse",
