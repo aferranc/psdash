b139000b67b9c24f867b4fd56fc0ffad76e3db29
Albert Ferran Casas <aferranc@cirsa.com>
2022-06-21 10:34:25 +0200

    change default port

--- psdash/run.py
+++ psdash/run.py
@@ -23,7 +23,7 @@ class PsDashRunner(object):
     DEFAULT_NET_IO_COUNTER_INTERVAL = 3
     DEFAULT_REGISTER_INTERVAL = 60
     DEFAULT_BIND_HOST = "0.0.0.0"
-    DEFAULT_PORT = 5000
+    DEFAULT_PORT = 8000
     LOCAL_NODE = "localhost"
 
     @classmethod
